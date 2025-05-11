97c49a3cc3690c53f8d52f3825237e49e089aeba
svetoslav <svetoslav@svetoslavs-MacBook-Pro.local>
2025-05-11 23:31:51 +0100

    updated publications page

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -68,36 +68,11 @@ for row, item in publications.iterrows():
     html_filename = str(item.pub_date) + "-" + item.url_slug
     year = item.pub_date[:4]
     
-    ## YAML variables
-    
     md = "---\ntitle: \""   + item.title + '"\n'
-    
-    md += """collection: publications"""
-    
-    md += """\npermalink: /publication/""" + html_filename
-    
-    if len(str(item.excerpt)) > 5:
-        md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
-    
-    md += "\ndate: " + str(item.pub_date) 
-    
-    md += "\nvenue: '" + html_escape(item.venue) + "'"
     
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
     
-    md += "\ncitation: '" + html_escape(item.citation) + "'"
-    
-    md += "\n---"
-    
-    ## Markdown description for individual page
-    
-    if len(str(item.paper_url)) > 5:
-        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
-        
-    if len(str(item.excerpt)) > 5:
-        md += "\n" + html_escape(item.excerpt) + "\n"
-        
     md += "\n" + item.citation
     
     md_filename = os.path.basename(md_filename)
